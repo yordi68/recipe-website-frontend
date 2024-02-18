7283e0e9592a3e9209a37bf7587847120f675e3f
yordi68 <yordanos.yirgu1@gmail.com>
2024-02-18 08:22:24 +0300

    make dish display cards

--- pages/recipes/index.vue
+++ pages/recipes/index.vue
@@ -1,66 +1,22 @@
 <template>
-    <div class="flex flex-col w-80 font-family-poppins">
-        <div class="w-80 h-60">
-            <img src="/pasta-1.jpg" alt="" class="w-full h-full"/>
 
-        </div>
-
-        <div class="w-11/12 space-y-2">
-
-            <div class="">
-
-            </div>
-
-            <div class="flex space-x-2 text-gray-500 items-center text-center">
-                <div class="w-4 h-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
-                        stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round"
-                            d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-                    </svg>
-                </div>
-
-                <p> <span class="underline text-sm font-family-poppins"> Muffin Group</span> at</p>
-                <div class="w-4 h-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
-                        stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round"
-                            d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                    </svg>
-                </div>
-                <p class="text-sm">July 12, 2018</p>
-            </div>
-
-            <h3 class="text-md font-semibold">Spaghetti with tomato sauce</h3>
-
-            <p class="text-xs">Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula. Ut molestie ultricies porta
-                urna
-            </p>
-
-            <div class="flex justify-between bg-gray-100 p-2">
-                <div class="flex justify-between">
-                    <div class="w-6 h-6 text-[#20c05c]">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-                        </svg>
-                    </div>
-                    <p>63</p>
-                </div>
-
-                <div class="flex justify-between">
-                    <div class="w-6 h-6">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z" />
-                        </svg>
-                    </div>
-                    <p class="underline text-[#20c05c]">Read More</p>
+    <div class="bg-cover bg-center h-64 relative" style="background-image: url('/background-recipe.jpg');">
+        <p class="text-sm text-[]">RECIPES</p>
+        <h1 class="text-3xl font-bold">Discover our amazing dishes!</h1>
+    </div>
 
 
-                </div>
-            </div>
 
 
-        </div>
+
+    <div class="grid grid-cols-3 gap-y-8 my-10">
+        <Card />
+        <Card />
+        <Card />
+
+        <Card />
+        <Card />
+        <Card />
     </div>
 </template>
 
