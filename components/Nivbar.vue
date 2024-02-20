7779b4d95f786d4ffc8764dd0a383090feae67af
yordi68 <yordanos.yirgu1@gmail.com>
2024-02-20 13:44:34 +0300

    update navbar

---
+++ components/Nivbar.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+      <nav class="relative px-4 py-4 flex justify-between items-center bg-black">
+
+        <!-- Your logo SVG -->
+        <NuxtLink to="/home" class="text-3xl font-bold leading-none">
+          <!-- Your SVG code for logo here -->
+          <img src="/berecipes.png" alt="">
+        </NuxtLink>
+
+
+
+        <div class="lg:hidden">
+          <!-- Button for mobile menu -->
+          <button class="navbar-burger flex items-center text-blue-600 p-3" @click="toggleMenu">
+            <svg class="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <title>Mobile menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+            </svg>
+          </button>
+        </div>
+
+        <!-- Your menu items -->
+        <ul v-if="isMenuOpen" class="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
+          <li><NuxtLink to="/" class="text-sm text-gray-400 hover:text-gray-500">Home</NuxtLink></li>
+          <li><NuxtLink to="/about" class="text-sm text-blue-600 font-bold">About Us</NuxtLink></li>
+          <li><NuxtLink to="/services" class="text-sm text-gray-400 hover:text-gray-500">Services</NuxtLink></li>
+          <li><NuxtLink to="/pricing" class="text-sm text-gray-400 hover:text-gray-500">Pricing</NuxtLink></li>
+          <li><NuxtLink to="/contact" class="text-sm text-gray-400 hover:text-gray-500">Contact</NuxtLink></li>
+        </ul>
+        <!-- Sign In and Sign Up buttons -->
+        <NuxtLink to="/signin" class="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold rounded-xl transition duration-200">Sign In</NuxtLink>
+        <NuxtLink to="/signup" class="hidden lg:inline-block py-2 px-6 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200">Sign up</NuxtLink>
+      </nav>
+    </div>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+
+    const isMenuOpen = ref(true);
+
+    const toggleMenu = () => {
+    isMenuOpen.value = !isMenuOpen.value;
+    };
+</script>
