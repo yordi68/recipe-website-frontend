a47612469b39b92b30bf6ad32579f36fd2d91ea4
yordi68 <yordanos.yirgu1@gmail.com>
2024-02-20 11:47:00 +0300

    make hamburger menu on mobile devices

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <nav class="sticky top-0 z-10 w-full h-20 bg-[#1f1d27] flex justify-between items-center px-20">
+    <nav class="sticky top-0 z-10 w-full h-20 bg-black flex justify-between items-center px-20">
         <div>
             <NuxtLink to="/home">
                 <img src="/berecipes.png" alt="">
@@ -21,29 +21,28 @@
             <li>Sign In</li>
         </ul>
 
-        <!-- Mobile Menu or Hamburger Menu  -->
-        <div class="md:hidden flex items-center">
-            <button @click="isOpen = !isOpen" class="md:hidden">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-white">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                </svg>
-            </button>
-        </div>
+    
+    <!-- Hamburger Button -->
+    <div class="md:hidden flex items-center">
+      <button @click="isOpen = !isOpen" class="md:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-6 h-6 text-white">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+      </button>
+    </div>
 
-        <!-- Mobile Navigation -->
-        <ul class="md:hidden" :class="isOpen ? 'block' : 'hidden'">
-            <NuxtLink to="/home" exact-active-class="text-extrabold text-[#20c05c]">
-                <li class="hover:text-[#20c05c]">Home</li>
-            </NuxtLink>
-
-            <NuxtLink to="/recipes" exact-active-class="text-extrabold text-[#20c05c]">
-                <li class="hover:text-[#20c05c]">Recipes</li>
-            </NuxtLink>
-
-            <NuxtLink to="/about" exact-active-class="text-extrabold text-[#20c05c]">
-                <li class="hover:text-[#20c05c]">About us</li>
-            </NuxtLink>
-        </ul>
+    <!-- Mobile Navigation -->
+    <ul :class="{ 'block': isOpen, 'hidden': !isOpen }" class="md:hidden text-white">
+      <li>
+        <NuxtLink to="/home" exact-active-class="text-extrabold text-[#20c05c]">Home</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/recipes" exact-active-class="text-extrabold text-[#20c05c]">Recipes</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/about" exact-active-class="text-extrabold text-[#20c05c]">About us</NuxtLink>
+      </li>
+    </ul>
     
 
     </nav>
